33c20cbedf3cae2d4edb2cfcc6fa3415bac22243
Praveen00114 <praveenhalundi47@gmail.com>
2024-02-29 14:56:53 +0530

    Done

--- Calci.java
+++ Calci.java
@@ -3,7 +3,7 @@ void int getSum(int n1,int n2);
 }
 
 class Add implements Calci{
-    public int getSum(int n1,int n2){
+    public int getSums(int n1,int n2){
         int sum=n1+n2;
         return sum;
     }
